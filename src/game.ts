85f50e52ff644e10f9c0d3d5e7e514e0bf906b91
toro09 <toro09@icloud.com>
2020-09-19 21:17:05 -0700

    moved tree again

--- src/game.ts
+++ src/game.ts
@@ -27,7 +27,7 @@ const bloomingPinkAcaciaTree = new Entity('bloomingPinkAcaciaTree')
 engine.addEntity(bloomingPinkAcaciaTree)
 bloomingPinkAcaciaTree.setParent(_scene)
 const transform3 = new Transform({
-  position: new Vector3(7, 0, 3),
+  position: new Vector3(5, 0, 5),
   rotation: new Quaternion(0, 0, 0, 1),
   scale: new Vector3(1, 1, 1)
 })
